8b631adbe370f2c007d4a7a39ca79bb6bcb8ffdf
dqn <dqn270@gmail.com>
2021-12-26 14:19:02 +0900

    chore: add theme color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import tailwindCssConfig from "../../tailwind.config.js";
+
+const brandColor = tailwindCssConfig.theme.colors.brand;
 
 class MyDocument extends Document {
   override render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <meta name="theme-color" content={brandColor} />
+        </Head>
+
         <body>
           <Main />
           <NextScript />
