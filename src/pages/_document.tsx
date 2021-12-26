f55032d38dc5ca0a06e5d9f1463ebe39ab7a8d88
dqn <dqn270@gmail.com>
2021-12-26 14:30:25 +0900

    fix: fix icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,19 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <meta name="theme-color" content={brandColor} />
+          <link rel="icon" href="favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            href="/apple-touch-icon.png"
+            type="image/png"
+            sizes="180x180"
+          />
+          <link
+            rel="icon"
+            href="/android-chrome.png"
+            type="image/png"
+            sizes="192x192"
+          />
         </Head>
 
         <body>
