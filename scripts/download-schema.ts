f5744c064d94dbd1f3c359c71f630450dc069545
dqn <dqn270@gmail.com>
2023-03-21 16:53:03 +0900

    fix: fix download-schema.ts

--- scripts/download-schema.ts
+++ scripts/download-schema.ts
@@ -1,20 +1,13 @@
-import dedent from "dedent";
 import { writeFile } from "fs/promises";
 
 const dist = "./src/schema.graphql";
 const url =
-  "https://raw.githubusercontent.com/dqn/api.dqn.me/main/src/app/graphql/typeDefs.ts";
+  "https://raw.githubusercontent.com/dqn/api.dqn.me/main/src/schema.graphql";
 
 async function main(): Promise<void> {
   const res = await fetch(url);
-  const text = await res.text();
-  const schema = text.match(/(type .+})/s)?.[1];
-
-  if (schema === undefined) {
-    throw new Error("invalid schema");
-  }
-
-  await writeFile(dist, dedent`${schema}` + "\n");
+  const schema = await res.text();
+  await writeFile(dist, schema);
 }
 
 main().catch((err) => {
